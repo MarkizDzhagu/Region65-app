b85b122221bbe88ab1729f5b30363cc2d222cbc5
MarkizDzhagu <77718554+MarkizDzhagu@users.noreply.github.com>
2023-02-13 18:26:33 +0400

    Add files via upload

---
+++ js/bundle.js
@@ -0,0 +1,169 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./css/company.css":
+/*!*************************!*\
+  !*** ./css/company.css ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./css/company.css?");
+
+/***/ }),
+
+/***/ "./css/delivery.css":
+/*!**************************!*\
+  !*** ./css/delivery.css ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./css/delivery.css?");
+
+/***/ }),
+
+/***/ "./css/footer.css":
+/*!************************!*\
+  !*** ./css/footer.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./css/footer.css?");
+
+/***/ }),
+
+/***/ "./css/header.css":
+/*!************************!*\
+  !*** ./css/header.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./css/header.css?");
+
+/***/ }),
+
+/***/ "./css/main.css":
+/*!**********************!*\
+  !*** ./css/main.css ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./css/main.css?");
+
+/***/ }),
+
+/***/ "./css/payment.css":
+/*!*************************!*\
+  !*** ./css/payment.css ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./css/payment.css?");
+
+/***/ }),
+
+/***/ "./css/popup.css":
+/*!***********************!*\
+  !*** ./css/popup.css ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./css/popup.css?");
+
+/***/ }),
+
+/***/ "./css/price.css":
+/*!***********************!*\
+  !*** ./css/price.css ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./css/price.css?");
+
+/***/ }),
+
+/***/ "./css/shop.css":
+/*!**********************!*\
+  !*** ./css/shop.css ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./css/shop.css?");
+
+/***/ }),
+
+/***/ "./css/styles.css":
+/*!************************!*\
+  !*** ./css/styles.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./css/styles.css?");
+
+/***/ }),
+
+/***/ "./script/main.js":
+/*!************************!*\
+  !*** ./script/main.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css_styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../css/styles.css */ \"./css/styles.css\");\n/* harmony import */ var _css_header_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../css/header.css */ \"./css/header.css\");\n/* harmony import */ var _css_main_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../css/main.css */ \"./css/main.css\");\n/* harmony import */ var _css_footer_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../css/footer.css */ \"./css/footer.css\");\n/* harmony import */ var _css_popup_css__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../css/popup.css */ \"./css/popup.css\");\n/* harmony import */ var _css_shop_css__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../css/shop.css */ \"./css/shop.css\");\n/* harmony import */ var _css_company_css__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../css/company.css */ \"./css/company.css\");\n/* harmony import */ var _css_price_css__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../css/price.css */ \"./css/price.css\");\n/* harmony import */ var _css_payment_css__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../css/payment.css */ \"./css/payment.css\");\n/* harmony import */ var _css_delivery_css__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ../css/delivery.css */ \"./css/delivery.css\");\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\ndocument.querySelector('.menu-icon-wrapper').onclick = function () {\r\n    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');\r\n    document.querySelector('.header__list').classList.toggle('_active');\r\n    document.body.classList.toggle('_lock');\r\n}\r\n\r\nymaps.ready(init);\r\n    function init(){\r\n        // Создание карты.\r\n        var myMap = new ymaps.Map(\"map\", {\r\n            // Координаты центра карты.\r\n            // Порядок по умолчанию: «широта, долгота».\r\n            // Чтобы не определять координаты центра карты вручную,\r\n            // воспользуйтесь инструментом Определение координат.\r\n            center: [46.957771, 142.729587],\r\n            // Уровень масштабирования. Допустимые значения:\r\n            // от 0 (весь мир) до 19.\r\n            zoom: 16\r\n        });\r\n        var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {\r\n            balloonContentBody: [\r\n                '<address>',\r\n                '<strong>Офис Яндекса в Москве</strong>',\r\n                '<br/>',\r\n                'Адрес: 119021, Москва, ул. Льва Толстого, 16',\r\n                '<br/>',\r\n                'Подробнее: <a href=\"https://company.yandex.ru/\">https://company.yandex.ru</a>',\r\n                '</address>'\r\n            ].join('')\r\n        }, {\r\n            preset: 'islands#redDotIcon'\r\n        });\r\n    \r\n        myMap.geoObjects.add(myPlacemark);\r\n    }\r\n\r\nnew Swiper('.image-slider', {\r\n    navigation: {\r\n        nextEl: '.swiper-button-next',\r\n        prevEl: '.swiper-button-prev'\r\n    },\r\n    pagination: {\r\n        el: '.swiper-pagination',\r\n        type: 'fraction',\r\n    },\r\n    loop: \"true\",\r\n});\r\n\r\nvar container = document.getElementById('container-distance');\r\nvar containerDistance = container.offsetLeft;\r\n\r\nvar distance = document.getElementById('333');\r\nvar distance2 = document.getElementById('444');\r\nvar distance3 = document.getElementById('555');\r\nvar distance4 = document.getElementById('666');\r\nvar distance5 = document.querySelector('.swiper-pagination-fraction');\r\ndistance.style.marginLeft = containerDistance + 'px';\r\ndistance2.style.marginLeft = containerDistance + 'px';\r\ndistance3.style.marginLeft = containerDistance + 'px';\r\ndistance4.style.marginLeft = containerDistance + 'px';\r\ndistance5.style.left = containerDistance + 'px';\r\n\r\n\r\ndocument.querySelector('.header__search').onclick = function () {\r\n    document.querySelector('.header__search').classList.toggle('AZAZA');\r\n}\r\n\r\n\r\n\r\n\n\n//# sourceURL=webpack:///./script/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./script/main.js");
+/******/ 	
+/******/ })()
+;
